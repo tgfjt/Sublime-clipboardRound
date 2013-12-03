d3f2336072d513dbbfee8a651c4b5b30caa7795a
tgfjt <fujita@tagpanda.co.jp>
2013-12-03 13:30:26 +0900

    dont make cache...

--- clipboardround.py
+++ clipboardround.py
@@ -1,21 +1,42 @@
 import sublime, sublime_plugin
-import codecs, os
+import subprocess
 
 history = []
-load_history = []
 menuitems = []
 history_index = 0
-flag = False
+data = 0
 
-__file__ = os.path.normpath(os.path.abspath(__file__))
-__path__ = os.path.dirname(__file__)
+def setClipboardHistory():
+    global history_index, menuitems, history, data
+    p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
+    retcode = p.wait()
+    clip = p.stdout.read()
+    data = clip.decode()
 
-CACHE_PATH = __path__ + '/.cache/cache'
+    if data in history:
+        return None
+    elif data == '':
+        return None
 
-f = codecs.open(CACHE_PATH, 'r', 'utf-8')
-str = f.read()
-load_history = str.split('\n********************\n')
-f.close()
+    settings = sublime.load_settings('Sublime-clipboardRound.sublime-settings')
+    limit = settings.get('limit')
+
+    if not history or history[0] != data:
+        history.insert(0, data)
+        history_index = 0
+        menuitems = history
+
+    if limit < len(history):
+        for i in xrange(len(history) - limit):
+            history.pop()
+            menuitems.pop()
+
+    return None
+
+def pasteClipboardHistory(self, text):
+    self.view.run_command('undo')
+    self.view.run_command('paste')
+    sublime.set_clipboard(text)
 
 class Clip_round_showCommand(sublime_plugin.TextCommand):
     def on_chosen(self, index):
@@ -29,106 +50,50 @@ class Clip_round_showCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         global menuitems
-        if load_history != ['']:
-            menuitems = load_history
-
         print(menuitems)
+        if menuitems == []:
+            return None
 
         self.view.window().show_quick_panel(menuitems, self.on_chosen, sublime.MONOSPACE_FONT)
 
 class Clip_round_prevCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         global history_index
-        if history and flag == True:
+        if history:
             clip = sublime.get_clipboard()
             history_index = min(history_index + 1, len(history) - 1)
             sublime.set_clipboard(history[history_index])
 
             sublime.set_timeout(lambda:
-                self.view.run_command('undo'), 0)
-
-            sublime.set_timeout(lambda:
-                self.view.run_command('paste'), 1)
-
-            sublime.set_timeout(lambda:
-                sublime.set_clipboard(clip), 2)
+                pasteClipboardHistory(self, clip), 0)
 
 class Clip_round_nextCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         global history_index
-        if history and flag == True:
+        if history:
             clip = sublime.get_clipboard()
             history_index = max(history_index - 1, 0)
             sublime.set_clipboard(history[history_index])
 
             sublime.set_timeout(lambda:
-                self.view.run_command('undo'), 0)
-
-            sublime.set_timeout(lambda:
-                self.view.run_command('paste'), 1)
-
-            sublime.set_timeout(lambda:
-                sublime.set_clipboard(clip), 2)
+                pasteClipboardHistory(self, clip), 0)
 
 class Clip_round_clearCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        global menuitems, history, history_index
+        global history, history_index, menuitems, data
         del menuitems[:]
         del history[:]
         history_index = 0
+        data = 0
+        sublime.set_clipboard('')
         print('clipboardRound: clear Clipboard History.')
 
 class ClipboardRoundListener(sublime_plugin.EventListener):
-    def on_query_context(self, view, key, operator, operand, match_all):
-        global history_index, flag
-
-        vi_copy = False
-
-        if operand == 'vi_copy' or operand == 'vi_delete':
-            vi_copy = True
-
-        elif key == 'clipboard_round_paste':
-            flag = True
-            return None
-        elif key != 'clipboard_round':
-            if vi_copy == False:
-                flag = False
-                return None
-
-        settings = sublime.load_settings('Sublime-clipboardRound.sublime-settings')
-        limit = settings.get('limit')
-
-        for selected in view.sel():
-            selected = view.sel()[0]
-            if selected.empty():
-                selected = view.line(selected)
-
-            text = view.substr(selected)
-
-            if not history or history[0] != text:
-                history.insert(0, text)
-                history_index = 0
-
-            if not menuitems or menuitems[0] != text:
-                menuitems.insert(0, text)
-
-        if limit < len(history):
-            for i in xrange(len(history) - limit):
-                history.pop()
-                menuitems.pop()
-
+    def on_query_context(self, view, *args):
+        sublime.set_timeout(lambda:
+            setClipboardHistory(), 0)
         return None
 
-    def on_load(self, view):
-        global load_history
-        f = codecs.open(CACHE_PATH, 'wr', 'utf-8')
-        contents = f.read()
-        f.write('')
-        load_history = contents.split('\n********************\n')
-        f.close()
-
-    def on_pre_save(self, view):
-        savelist = '\n********************\n'.join(history)
-        f = codecs.open(CACHE_PATH, 'wb', 'utf-8')
-        f.write(savelist)
-        f.close()
+    def on_text_command(self, view, command, *args):
+        sublime.set_timeout(lambda:
+            setClipboardHistory(), 0)
